3364ae6711856286c82de40a939f721ce6fe2941
Shaikh Habibur Rehaman <s.h.rehaman.art@gmail.com>
2023-04-19 20:36:44 +0530

    Database Connection Started

--- app/src/main/java/com/example/project_test/MainActivity3.java
+++ app/src/main/java/com/example/project_test/MainActivity3.java
@@ -7,14 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity3 extends AppCompatActivity {
+    private FirebaseAuth auth ;
+    private EditText signupemail, signuppw;
  Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         button = findViewById(R.id.next_btn);
+        signupemail = findViewById(R.id.email_id);
+        signuppw= findViewById(R.id.PW);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
